07795edbbf9b85fe96255f07138a8c1dac03ca7e
Jadhav-Radhika <you@example.com>
2020-05-26 19:32:24 +0530

    Project Files Uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FooterComponent} from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { OffersComponent } from './Components/offers/offers.component';
+import { CustomerReviewComponent } from './Components/customer-review/customer-review.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{
+    path:'',
+    component:FooterComponent
+  },*/
+   
+  {
+    path:'home',
+    component:HeaderComponent
+  },
+  { path:'offers', component:OffersComponent},
+  { path:'customer-review', component:CustomerReviewComponent},
+  { path:'footer', component:FooterComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OffersComponent } from './Components/offers/offers.component';
+import { CustomerReviewComponent } from './Components/customer-review/customer-review.component';
+import { HeaderComponent} from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OffersComponent,
+    CustomerReviewComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
